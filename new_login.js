5a2674ce2fdb57156ad4cc0241f7ebe3c6c70fd6
Leejonghyeok0112 <dlwhdgur112@naver.com>
2024-03-02 01:26:49 +0900

    today

---
+++ new_login.js
@@ -0,0 +1,17 @@
+NodeList.prototype.map= Array.prototype.map;
+const all = document.querySelector("input.all");
+const terms = document.querySelectorAll("input.term");
+
+all.addEventListener("click", () => {
+    terms.forEach((term) =>{
+        term.checked = all. checked;
+    });
+});
+
+terms.forEach((term) => {
+    term.addEventListener("click", (e) => {
+        all.checked =
+            terms.map((term) => term.checked).filter((checked) => checked)
+                .length === 6;
+    });
+});
